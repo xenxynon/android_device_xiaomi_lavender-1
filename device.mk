7d3c63cd87b4d61dc6b20a9fb7a0e04f74b71e97
xenxynon <flynryder427@gmail.com>
2022-07-07 10:18:06 +0530

    lavender: debloat
    Signed-off-by: xenxynon <flynryder427@gmail.com>

--- device.mk
+++ device.mk
@@ -352,6 +352,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libjson
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.class_main.sh \
