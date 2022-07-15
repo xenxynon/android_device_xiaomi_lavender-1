85c5c726fb321897b14aeb1aa82d1973645a6c58
xenxynon <flynryder427@gmail.com>
2022-07-15 10:52:23 +0530

    fusify
    Signed-off-by: xenxynon <flynryder427@gmail.com>

--- fuse_lavender.mk
+++ fuse_lavender.mk
@@ -8,15 +8,15 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Proton stuff
-$(call inherit-product, vendor/proton/build/common.mk)
+# Inherit some stuff
+$(call inherit-product, vendor/fuse/config/common.mk)
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 $(call inherit-product, $(LOCAL_PATH)/device-hidl.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_lavender
+PRODUCT_NAME := fuse_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
