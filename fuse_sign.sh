108780ac1e0d2fcaeb08345288d08c0928925dbb
Mayur <flynryder427@gmail.com>
2022-07-16 15:49:12 +0530

    sign: some changes

--- fuse_sign.sh
+++ fuse_sign.sh
@@ -1,12 +1,27 @@
+source build/envsetup.sh
+
+# ccache
+
+# override metadata
+export WITH_GAPPS=true
+export KBUILD_BUILD_USER=xenxynon 
+export KBUILD_BUILD_HOST=xenxynon 
+export BUILD_USERNAME=xenxynon
+export BUILD_HOSTNAME=xenxynon
+
+# build it
+lunch fuse_lavender-user
+export WITH_GAPPS=true
 make target-files-package otatools
 
-sign_target_files_apks -o -d certs \
+# sign it
+croot
+sign_target_files_apks -o -d keys \
     $OUT/obj/PACKAGING/target_files_intermediates/*-target_files-*.zip \
     signed-target_files.zip
 
-
-ota_from_target_files \
-signed-target_files.zip \
-fusionOS-lavender-signed.zip
-
-mv fusionOS-lavender-signed.zip $OUT
+ota_from_target_files -k keys/releasekey \
+    signed-target_files.zip \
+$PROD_VERSION.zip
+cp $PROD_VERSION.zip $OUT
+rm *zip
