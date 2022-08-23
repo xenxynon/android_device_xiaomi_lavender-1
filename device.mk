36ae135a62ad2a51cbad6f935bb25a6b7e1ab29a
xenxynon <flynryder427@gmail.com>
2022-08-23 17:43:28 +0000

    Revert "lavender: debloat"
    This reverts commit 7d3c63cd87b4d61dc6b20a9fb7a0e04f74b71e97.

--- device.mk
+++ device.mk
@@ -352,10 +352,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libjson
 
-# Remove unwanted packages
-PRODUCT_PACKAGES += \
-    RemovePackages
-
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.class_main.sh \
