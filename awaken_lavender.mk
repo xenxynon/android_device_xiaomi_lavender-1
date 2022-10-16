7ef5f9e1802ebc134ddd8d7634540fbddead8db1
xenxynon <flynryder427@gmail.com>
2022-10-16 15:51:18 +0000

    AwakenOS
    Signed-off-by: xenxynon <flynryder427@gmail.com>

--- awaken_lavender.mk
+++ awaken_lavender.mk
@@ -8,15 +8,15 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Proton stuff
-$(call inherit-product, vendor/proton/build/common.mk)
+# Inherit custom AwakenOS vendor configuration
+$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 $(call inherit-product, $(LOCAL_PATH)/device-hidl.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_lavender
+PRODUCT_NAME := awaken_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
@@ -26,3 +26,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="lavender" \
     PRODUCT_NAME="lavender"
+# some things
+USE_PIXEL_CHARGER := true
+TARGET_SUPPORTS_QUICK_TAP := true
